725ebf632ed5a5e617e8afd83b5466a641ec477f
Henry To <115491088+henry-to@users.noreply.github.com>
2025-04-30 12:07:16 -0700

    Initial Commit

---
+++ FiTTTTv1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FiTTTTv1
+//
+//  Created by Henry To on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FiTTTTv1/FiTTTTv1App.swift
@@ -0,0 +1,17 @@
+//
+//  FiTTTTv1App.swift
+//  FiTTTTv1
+//
+//  Created by Henry To on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FiTTTTv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
